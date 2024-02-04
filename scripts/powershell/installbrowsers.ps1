0c3d50a0e12a5b1c13375344a36bc90bfe775690
ericc168 <96398565+ericc168@users.noreply.github.com>
2024-02-04 11:19:45 +0800

    Update installbrowsers.ps1

--- scripts/powershell/installbrowsers.ps1
+++ scripts/powershell/installbrowsers.ps1
@@ -1,9 +1,9 @@
 # Create temp folder for downloading installers
-md -Path $env:temp\installers -erroraction SilentlyContinue | Out-Null
+md -Path c:\temp\installers -erroraction SilentlyContinue | Out-Null
 # Install Chrome
 # Define the download URL and the installer
 $chromeUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
-$installer = join-path $env:temp\installers chrome_installer.exe
+$installer = join-path c:\temp\installers chrome_installer.exe
 
 # Download Google Chrome installer
 Invoke-WebRequest -Uri $chromeUrl -OutFile $installer
@@ -16,7 +16,7 @@ Start-Process -FilePath $installer -ArgumentList "/silent /install" -Wait
 
 # Install Edge Browser
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-$Download = join-path $env:temp\install MicrosoftEdgeEnterpriseX64.msi
+$Download = join-path c:\temp\install MicrosoftEdgeEnterpriseX64.msi
 $edgeUrl = "http://go.microsoft.com/fwlink/?LinkID=2093437"
 # the URL comes from https://techcommunity.microsoft.com/t5/discussions/official-download-links-for-microsoft-edge-stable-enterprise/m-p/1082549
 Invoke-WebRequest -Uri $edgeUrl  -OutFile $Download
