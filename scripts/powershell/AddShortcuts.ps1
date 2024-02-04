d2e789fded85cf42bc9f770d62c76f7adadb6c5f
ericc168 <96398565+ericc168@users.noreply.github.com>
2024-02-04 15:21:05 +0800

    Update AddShortcuts.ps1

--- scripts/powershell/AddShortcuts.ps1
+++ scripts/powershell/AddShortcuts.ps1
@@ -15,3 +15,23 @@ $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
 $Shortcut.Arguments = "https://signin.v1.trendmicro.com/"
 $Shortcut.Save()
+
+# Disable Edge fist-run wizard
+
+$settings = 
+[PSCustomObject]@{
+    Path  = "SOFTWARE\Policies\Microsoft\Edge"
+    Value = 1
+    Name  = "HideFirstRunExperience"
+} | group Path
+
+foreach($setting in $settings){
+    $registry = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey($setting.Name, $true)
+    if ($null -eq $registry) {
+        $registry = [Microsoft.Win32.Registry]::LocalMachine.CreateSubKey($setting.Name, $true)
+    }
+    $setting.Group | %{
+        $registry.SetValue($_.name, $_.value)
+    }
+    $registry.Dispose()
+}
